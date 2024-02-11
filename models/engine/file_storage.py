348338e929a3ea9afb892905d7368478c2fd075c
saharrrrr <skmao23@gmail.com>
2024-02-11 02:15:38 -0800

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-"""Define the file storage class."""
+"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
 from models.city import City
 from models.place import Place
-from models.state import State
 from models.amenity import Amenity
 from models.review import Review
 
 
 class FileStorage:
-    """class that serializes instances to a JSON file
-    and deserializes JSON file to instances.
+    """Represent an abstracted storage engine.
 
     Attributes:
-    __file_path: string - path to the JSON file.
-    __objects: the dictionary that will store all objects.
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects."""
+        """Return the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key."""
-        obj_key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[obj_key] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w") as file:
-            json.dump(FileStorage.__file_path, file)
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
         """deserializes the JSON file to __objects."""

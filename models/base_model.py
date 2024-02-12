6c6f90bec31a7bff25dbd13392df5ce0318c02f5
saharrrrr <skmao23@gmail.com>
2024-02-11 22:24:22 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
-"""Define BaseModel Class."""
-from uuid import uuid4
+"""Define The Base Class"""
+import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    """The Basemodel of the HBnB Project."""
-
-    def __init__(self, *args, **kwargs):
+    """The main class for the project
+    all other classes will inherant from it."""
+    def __init__(self):
         """Initialize a new BaseModel.
         Attributes:
             *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes."""
-
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if len(kwargs) != 0:
-            for i, j in kwargs.items():
-                if i == "created_at" or j == "updated_at":
-                    self.__dic__[i] = datetime.strptime(j, time_format)
-                else:
-                    self.__dict__[i] = j
-        else:
-            models.storage.save()
+            **kwargs (dict): Key/value pairs of attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-
-        cls_name = self.__class__.__name__
-        print(f"{cls_name} {self.id} {self.__dict__}")
+        """print: [<class name>] (<self.id>) <self.__dict__>"""
+        print(f"{self.__class__.__name__} {self.id} {self.__dict__}")
 
     def save(self):
-        """updates updated_at with the current datetime"""
-
-        self.updated_at = datetime.now()
-        models.storage.save()
+        """updates the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.now
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values"""
-
-        dict1 = self.__dict__.copy()
-        dict1["created_at"] = self.created_at.isoformat()
-        dict1["updated_at"] = self.updated_at.isoformat()
-        dict1["__class__"] = self.__class__.__name__
-        return dict1
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance."""
+        obj_dict = {}
+        for key, value in self.__dict__.item():
+            if isinstance(value, datetime):
+                obj_dict[key] = value.isoformat()
+            else:
+                obj_dict[key] = value
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict

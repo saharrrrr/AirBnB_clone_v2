766ec295752215bcd3b915adf2e3b813c77e2e1c
saharrrrr <skmao23@gmail.com>
2024-02-11 22:39:13 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class BaseModel:
     """The main class for the project
     all other classes will inherant from it."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
         Attributes:
             *args (any): Unused.
@@ -15,6 +15,13 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""

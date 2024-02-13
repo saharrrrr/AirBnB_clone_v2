d42b714bd4ff84784cd1e151f007a9ed67650cd0
saharrrrr <skmao23@gmail.com>
2024-02-13 03:26:39 -0800

    updated

--- console.py
+++ console.py
@@ -31,13 +31,27 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def defult(self, arg):
+        """Default behavior for cmd module when input is invalid."""
         arg_dict = {
                 "all": self.do_all,
                 "show": self.do_show,
-                "destroy": self.do_destroy
+                "destroy": self.do_destroy,
+                "count": self.do_count,
+                "update": self.do_update
                 }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            args = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [args[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(args[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
-    def empty_line(self):
+    def emptyline(self):
         """Do nothing upon receiving an empty line."""
         pass
 

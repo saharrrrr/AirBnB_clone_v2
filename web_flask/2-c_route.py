f9892cb930f05125f4aaf6c3a494fa2255d8cb92
saharrrrr <skmao23@gmail.com>
2024-04-20 14:59:00 -0700

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_hbnb():
+    """ display “Hello HBNB!”"""
+    return("Hello HBNB")
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ display “HBNB” """
+    return("HBNB")
+
+
+@app.route("/c/<text>")
+def c(text):
+    """display “C ” followed by the value of the text variable"""
+    text = text.replace ("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
